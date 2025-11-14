b4e0f6962c69fb4acee5e5c741f740fad97b293f
PrajwolKhadka <prazolkhadka67@gmail.com>
2025-11-14 18:09:43 +0545

    Backend changes with mongodb

--- backend/models/question.js
+++ backend/models/question.js
@@ -1,33 +1,11 @@
-import pool from "../db.js";
+import mongoose from "mongoose";
 
-// export const getQuestionsByDifficulty = async (difficulty) => {
-//   const res = await pool.query(
-//     "SELECT * FROM questions WHERE difficulty = $1 ORDER BY RANDOM() LIMIT 1",
-//     [difficulty]
-//   );
-//   return res.rows[0];
-// };
-export const getQuestionsByDifficulty = async (difficulty, excludeIds = []) => {
-  let query = "SELECT * FROM questions WHERE difficulty = $1";
-  let params = [difficulty];
+const questionSchema = new mongoose.Schema({
+    schoolId: {type: mongoose.Schema.Types.ObjectId, ref: 'School', required: true},
+    question: {type: String, required: true},
+    correctAnswer : {type: String, required: true},
+    difficulty : {type : Number , enum : [0,1,2,3,4], default : 0},
+    createdAt: {type: Date, default: Date.now()},
+})
 
-  if (excludeIds.length > 0) {
-    const placeholders = excludeIds.map((_, i) => `$${i + 2}`).join(",");
-    query += ` AND id NOT IN (${placeholders})`;
-    params = [difficulty, ...excludeIds];
-  }
-
-  query += " ORDER BY RANDOM() LIMIT 1";
-
-  const res = await pool.query(query, params);
-  return res.rows[0] || null; // return null if no question left
-};
-
-export const seedQuestions = async (questions) => {
-  for (const q of questions) {
-    await pool.query(
-      "INSERT INTO questions (question_text, options, correct_option, difficulty) VALUES ($1, $2, $3, $4)",
-      [q.question_text, JSON.stringify(q.options), q.correct_option, q.difficulty]
-    );
-  }
-};
+export default mongoose.model("Question", questionSchema);
--- backend/models/result.js
+++ backend/models/result.js
@@ -15,3 +15,15 @@ export const getResultByUserId = async (userId) => {
   );
   return res.rows[0];
 };
+
+
+
+import mongoose from "mongoose";
+
+const resultSchema = new mongoose.schema({
+  studentId : {type: mongoose.Schema.Types.ObjectId, ref : 'Student', required : true},
+  score : {type : int, required: true},
+  pyschometricEvaluation : {type : String , required : false},
+})
+
+export default mongoose.model("Result", resultSchema);
