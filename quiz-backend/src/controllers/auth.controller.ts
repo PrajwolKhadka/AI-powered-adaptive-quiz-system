f0acd682944a7dd291d1d098b5978b3feba18dc9
PrajwolKhadka <prazolkhadka67@gmail.com>
2025-12-20 13:16:25 +0545

    try catch to previous logic

--- quiz-backend/src/controllers/auth.controller.ts
+++ quiz-backend/src/controllers/auth.controller.ts
@@ -4,15 +4,22 @@ import { loginDto } from "../dtos/auth.dto";
 import { AuthService } from "../services/auth.services";
 
 export const registerSchool = async (req: Request, res: Response) => {
+  try{
   const data = registerSchoolDto.parse(req.body);
   await AuthService.registerSchool(data);
   res.status(201).json({
     message: "Registration successful. Awaiting verification.",
   });
+}
+  catch(err:any){
+    res.status(400).json({error: err.message || "Registration failed"});
+  }
 };
 
 export const login = async (req: Request, res: Response) => {
+  try{
   const { email, password } = loginDto.parse(req.body);
   const result = await AuthService.login(email, password);
   res.json(result);
+  }catch(err:any){({error: err.message || "Login failed"});}
 };
