c74d4a5037a525343dd057ca61972b3f1e020d89
PrajwolKhadka <prazolkhadka67@gmail.com>
2026-01-26 18:57:58 +0545

    services for pp

---
+++ quiz-backend/src/services/student.service.ts
@@ -0,0 +1,21 @@
+import { Student } from "../models/student.model";
+
+export const StudentService = {
+  async uploadProfilePicture(studentId: string, imageUrl: string) {
+    const student = await Student.findById(studentId);
+    if (!student) {
+      throw new Error("Student not found");
+    }
+
+    // if (!student.isFirstLogin) {
+    //   throw new Error("Profile already completed");
+    // }
+
+    student.imageUrl = imageUrl;
+    student.isFirstLogin = false;
+
+    await student.save();
+
+    return student;
+  },
+};
