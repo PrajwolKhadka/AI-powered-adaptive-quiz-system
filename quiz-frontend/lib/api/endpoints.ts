a4e117888bc3564a6c554d7a8c51af76fb3df70e
PrajwolKhadka <prazolkhadka67@gmail.com>
2026-02-08 11:45:33 +0545

    feat(frontend): add new API endpoint constants

--- quiz-frontend/lib/api/endpoints.ts
+++ quiz-frontend/lib/api/endpoints.ts
@@ -1,13 +1,15 @@
-export const API ={
-    AUTH:{
-        REGISTER_SCHOOL : "/api/school/auth/register-school",
-        LOGIN: "/api/school/auth/login"
-    },
-    CRUD: {
-        CREATE_STUDENT : "/api/school/create-student",
-        UPDATE_STUDENT : "/api/school/students/:id",
-        DELETE_STUDENT : "/api/school/students/:id",
-        DELETE_BATCH_STUDENTS : "/api/school/students/delete-batch",
-        FETCH_STUDENTS : "/api/school/students"
-    }
-}
+export const API = {
+  AUTH: {
+    REGISTER_SCHOOL: "/api/school/auth/register-school",
+    LOGIN: "/api/school/auth/login",
+    FORGOT_PASSWORD: "/api/school/auth/forgot-password",
+    RESET_PASSWORD: "/api/school/auth/reset-password",
+  },
+  CRUD: {
+    CREATE_STUDENT: "/api/school/create-student",
+    UPDATE_STUDENT: "/api/school/students/:id",
+    DELETE_STUDENT: "/api/school/students/:id",
+    DELETE_BATCH_STUDENTS: "/api/school/students/delete-batch",
+    FETCH_STUDENTS: "/api/school/students",
+  },
+};
