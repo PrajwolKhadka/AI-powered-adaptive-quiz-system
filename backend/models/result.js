2df946cb5ebc2527a899cd31fb2acbdb535bb6f7
PrajwolKhadka <prazolkhadka67@gmail.com>
2026-02-08 11:50:45 +0545

    fix(delete): older backend delete

--- backend/models/result.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const resultSchema = new mongoose.schema({
-  studentId : {type: mongoose.Schema.Types.ObjectId, ref : 'Student', required : true},
-  score : {type : int, required: true},
-  pyschometricEvaluation : {type : String , required : false},
-})
-
-export default mongoose.model("Result", resultSchema);
